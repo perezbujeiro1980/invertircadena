e40da9bcb2c76bdcb301fcbb6674c599366a0cc8
githubawaq <97857484+perezbujeiro1980@users.noreply.github.com>
2023-04-04 15:01:46 -0300

    first commit

---
+++ invertirCadena.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //invertir cadena
+            //a,b,c,d,e
+            //e,d,c,b,a
+
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+           
+            for (int i = cars.Length-1; i >= 0; i--)
+            {
+              Console.WriteLine(cars[i]);    
+            }
+
+            Console.ReadLine();
+
+            
+            
+
+
+
+
+        }
+    }
+}
